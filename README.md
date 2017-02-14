38ad741417cc8e517fd324c4a0dca452e8052b5a
butlerx <cianbutlerx@gmail.com>
2017-02-14 15:09:10 +0000

    refactor to create dist with all resources

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Reveal.js slide deck
-Reveal.js slide decks for Redbrick
+Reveal.js slide decks for Redbrick  
+[demo](https://redbrick.github.io/reveal-slides/demo.html#/)
 
 ## To Use
  - `npm install`
  - Make slides in `/slides` by copying template.html
  - Do presentation
+
+## Talks
+ - [intro 2016](https://redbrick.github.io/reveal-slides/slides/intro2016.html)
+ - [Using Redbrick](https://redbrick.github.io/reveal-slides/slides/how_to_redbrick.html)
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
 
 gulp.task('copy-images', function () {
   return gulp.src('./theme/images/*')
@@ -9,7 +9,29 @@ gulp.task('copy-images', function () {
 gulp.task('sass', function () {
   return gulp.src('./theme/redbrick.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('default', ['copy-images', 'sass']);
+gulp.task('copy-css', function() {
+  return gulp.src([
+    './node_modules/reveal.js/css/print/pdf.css',
+    './node_modules/reveal.js/css/print/paper.css'
+    ])
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('copy-js', function() {
+  return gulp.src([
+    './node_modules/reveal.js/js/reveal.js',
+    './node_modules/reveal.js/lib/js/head.min.js',
+    './node_modules/reveal.js/lib/js/classList.js',
+    './node_modules/reveal.js/plugin/notes/notes.js',
+    './node_modules/reveal.js/plugin/zoom-js/zoom.js',
+    './node_modules/reveal.js/plugin/highlight/highlight.js',
+    './node_modules/reveal.js/plugin/markdown/markdown.js',
+    './node_modules/reveal.js/plugin/markdown/marked.js'
+    ])
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default', ['copy-images', 'copy-js', 'copy-css', 'sass']);
